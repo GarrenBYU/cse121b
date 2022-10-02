27e37116604fff2e9b75042f153b2ece1346e97d
GarrenBYU <mei21004@byui.edu>
2022-10-01 19:53:04 -0600

    year and equations working

--- week03/scripts/task3.js
+++ week03/scripts/task3.js
@@ -5,7 +5,7 @@
 // Step 1: Using function declaration, define a function named add that takes two arguments, number1 and number2
 function add(number1, number2){
     // Step 2: In the function, return the sum of the parameters number1 and number2
-    return number1 + number2
+    return Number(number1) + Number(number2)
 }
 // Step 3: Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
 function addNumbers(){
@@ -14,60 +14,63 @@ function addNumbers(){
     let newSum = add(numOne, numTwo);
     // Step 4: Assign the return value to an HTML form element with an ID of sum
     let sum = document.getElementById('sum');
-    document.querySelector('#sum').textContent = newSum;
+    return document.querySelector('#sum').value = newSum;
 }
 // Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
 const buttonElement = document.getElementById('addNumbers');
 buttonElement.addEventListener("click", addNumbers)
 // Step 6: Using function expressions, repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend, difference and subtractNumbers
 function subtract(number1, number2){
-    return number1 - number2
+    return Number(number1) - Number(number2)
 }
 
 function subtractNumbers(){
-    let numOne = document.getElementById('#minuend').value;
-    let numTwo = document.getElementById('#subtahend').value;
+    let numOne = document.getElementById('minuend').value;
+    let numTwo = document.getElementById('subtrahend').value;
     let newTotal = subtract(numOne, numTwo);
-    return document.querySelector('#difference')
+    return document.querySelector('#difference').value = newTotal
 }
-'#subtractNumbers'.addEventListener("click", subtractNumbers)
+const buttonElementSub = document.getElementById('subtractNumbers')
+buttonElementSub.addEventListener("click", subtractNumbers)
 // Step 7: Using arrow functions, repeat Steps 1-5 with new functions named multiply and mulitplyNumbers and HTML form controls with IDs of factor1, factor2, product and multiplyNumbers
 function multiply(number1, number2){
-    return number1 * number2
+    return Number(number1) * Number(number2)
 }
 
 function multiplyNumbers(){
-    let numOne = document.getElementById('#factor1');
-    let numTwo = document.getElementById('#factor2');
+    let numOne = document.getElementById('factor1').value;
+    let numTwo = document.getElementById('factor2').value;
     let newProduct = multiply(numOne, numTwo);
-    return document.querySelector('#product') = newProduct
+    return document.querySelector('#product').value = newProduct
 }
-'#multiplyNumbers'.addEventListener('click', multiplyNumbers)
+const buttonElementMult = document.getElementById('multiplyNumbers')
+buttonElementMult.addEventListener("click", multiplyNumbers)
 // Step 8: Using any of the three function declaration types, repeat Steps 1-5 with new functions named divide and divideNumbers and HTML form controls with IDs of dividend, divisor, quotient and divideNumbers
 function divide(number1, number2){
-    return number1 / number2
+    return Number(number1) / Number(number2)
 }
 
 function divideNumbers(){
-    let numOne = document.getElementById('#dividend');
-    let numTwo = document.getElementById('#divisor');
-    let newQuotient = numOne / numTwo;
-    return document.querySelector('#quotient') = newQuotient
+    let numOne = document.getElementById('dividend').value;
+    let numTwo = document.getElementById('divisor').value;
+    let newQuotient = divide(numOne, numTwo);
+    return document.querySelector('#quotient').value = newQuotient
 }
-'#divideNumbers'.addEventListener('click', divideNumbers)
+const buttonElementDiv = document.getElementById('divideNumbers');
+buttonElementDiv.addEventListener('click', divideNumbers)
 // Step 9: Test all of the mathematical functionality of the task3.html page.
 
 
 /* BUILT-IN METHODS */
 
 // Step 1: Declare and instantiate a variable of type Date to hold the current date
-
+const d = new Date();
 // Step 2: Declare a variable to hold the current year
-
+let y = ''
 // Step 3: Using the variable declared in Step 1, call the built-in getFullYear() method/function and assign it to the variable declared in Step 2
-
+y = d.getFullYear()
 // Step 4: Assign the current year variable to an HTML form element with an ID of year
-
+document.querySelector('#year').textContent = y
 
 /* ARRAY METHODS */
 
