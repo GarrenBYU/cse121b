16122d5b584859fd0b2b04517491f4ed43173de4
GarrenBYU <mei21004@byui.edu>
2022-10-24 22:05:28 -0600

    new form of BOM and updates to code

--- week05/scripts/main.js
+++ week05/scripts/main.js
@@ -8,6 +8,32 @@ document.querySelector('#year').textContent = y
 
 let scriptures = []
 
+function output(array){
+    array.forEach((scriptures) =>{
+        let article = document.createElement('article');
+        article.setAttribute('id', 'currentVerse')
+
+        let volumeName = document.createElement('h3');
+        volumeName.textContent = scriptures.volume_title;
+
+        let bookTitle = document.createElement('h4');
+        bookTitle.textContent = scriptures.book_title;
+
+        let chapterNumber = document.createElement('h4');
+        chapterNumber.textContent = scriptures.chapter_number;
+
+        let verseNumber = document.createElement('h4');
+        verseNumber.textContent = scriptures.verse_number;
+
+        article.appendChild(volumeName);
+        article.appendChild(bookTitle);
+        article.appendChild(chapterNumber);
+        article.appendChild(verseNumber);
+
+        document.querySelector('#scriptures').appendChild(article);
+    });
+};
+
 async function getScripture(url){
     const response = await fetch(url);
 
@@ -18,22 +44,5 @@ async function getScripture(url){
 }
 
 scriptures = (getScripture('https://www.churchofjesuschrist.org/study/scriptures/bofm/1-ne/1?lang=eng&id=p1#p1'))
-
-function output(array){
-    array.forEach((scriptures) =>{
-        let article = document.createElement('article');
-        article.setAttribute('id', 'currentVerse')
-
-        let scripture = document.createElement('p');
-        scripture.setAttribute ('src', scripture);
-        scripture.setAttribute('alt', "it didnt work");
-
-        article.appendChild(scripture);
-
-        document.querySelector('#scriptures').appendChild(article);
-    });
-};
-
-
 
 document.querySelector('#scriptures').textContent = scriptures;
